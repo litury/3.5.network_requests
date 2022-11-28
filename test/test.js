8ca9b79fd8abe8b550b5ddd08cfe78ee4c6c4fb0
litury <litury@icloud.com>
2022-11-28 14:58:47 +0500

    fix: XML start

--- test/test.js
+++ test/test.js
@@ -1,19 +1,1 @@
 
-const usersURL = 'https://jsonplaceholder.typicode.com/posts'
-const button = document.querySelector('.action-button')
-
-function sendRequest() {
-	return fetch(usersURL)
-		.then(response => response.json())
-		.then(data => ap)
-}
-
-
-async function testFunc() {
-	await sendRequest
-	console.log(sendRequest())
-}
-
-
-
-button.addEventListener('click',testFunc)
